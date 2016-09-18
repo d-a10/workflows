031130aef520f7b7cd9bf3adb158128c55db777f
Awesome Daniel <d_a10@txstate.edu>
2016-09-17 19:15:55 -0500

    added watch task

--- gulpfile.js
+++ gulpfile.js
@@ -41,5 +41,9 @@ gulp.task('compass', function() {
 
 gulp.task('default', ['coffee', 'js', 'compass'])
 
-
+gulp.task('watch', function() {
+	gulp.watch(coffeeSources, ['coffee']);//jsSources, sassSources)
+	gulp.watch(jsSources, ['js']);
+	gulp.watch('components/sass/*.scss', ['compass']);
+})
 
